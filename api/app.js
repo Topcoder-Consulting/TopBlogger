f8109fe75fe68f484f81bd40cb1e27bae833ddbf
Jeff Douglas <jeff@jeffdouglas.com>
2015-03-03 07:53:14 -0800

    initial commit

---
+++ api/app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.connect( process.env.MONGOLAB_URI|| 'mongodb://localhost:27017/topblogger' );
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Make sure MongoDB is running.');
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,13 @@
+# TopBlogger
+
+The application will consist of a web UI and API. We are building the API first.
+
+## API
+
+To contribute to the project, first fork the application. Find an issue that you would like to work on, create a new branch, implement the code and then send a pull request referencing the issue.
+
+Make sure you update frequently form the master repo to ensure you have the latest code.
+
+## Web
+
+More info to come.

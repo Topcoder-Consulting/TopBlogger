0f92a9cb048e1c76b74725975c5b2113decd306f
Jeff Douglas <jeff@jeffdouglas.com>
2015-03-13 12:33:32 -0700

    Model implemented by LOYEER closes #1

--- api/models/Blog.js
+++ api/models/Blog.js
@@ -1,9 +1,135 @@
 var mongoose = require('mongoose');
 
 var blogSchema = new mongoose.Schema({
-  title: String,
-  slug: String,
-  views: { type: Number, default: 0 }
+    _id: {
+        type: Number,
+        required: true,
+        index: true, 
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    publishedDate: {
+        type: Number,
+        required: false
+    },
+    createdDate: {
+        type: Number,
+        required: true
+    },
+    lastUpdateedDate: {
+        type: Number,
+        required: true
+    },
+    author: {
+        type: User,
+        required: true,
+        meta: {
+            _id: {
+                type: Number,
+                required: true,
+                index: true,
+                unique: true
+            },
+            handle: {
+                type: String,
+                required: true
+            }
+        }
+    },
+    tags: {
+        type: [Tag],
+        meta: {
+            _id: {
+                type: Number,
+                required: true,
+                index: true,
+                unique: true
+            },
+            name: {
+                type: String,
+                required: true
+            }
+        }
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    isPublished: {
+        type: Boolean,
+        required: true
+    },
+    comments: {
+        type: [Comment],
+        meta: {
+            _id: {
+                type: Number,
+                required: true,
+                index: true,
+                unique: true
+            },
+            content: {
+                type: String,
+                required: true
+            },
+            postedDate: {
+                type: Number,
+                required: true
+            },
+            lastUpdatedDate: {
+                type: Number,
+                required: true
+            },
+            author: {
+                type: User,
+                required: true,
+                meta: {
+                    _id: {
+                        type: Number,
+                        required: true,
+                        index: true,
+                        unique: true
+                    },
+                    handle: {
+                        type: String,
+                        required: true
+                    }
+                }
+            },
+            numOfLikes: {
+                type: Number,
+                required: true,
+                default: 0
+            },
+            numOfDislikes: {
+                type: Number,
+                required: true,
+                default: 0
+            }
+        }
+    },
+    numOfViews: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    numOfUpVotes: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    numOfDownVotes: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 });
 
 module.exports = mongoose.model('Blog', blogSchema);

ba13f9cee4448240f112d9b66aabe51d305335c1
colorfullyme <ask.colorfully.me@gmail.com>
2015-04-02 08:05:00 +0300

    Add 200 mocha test for /api/secret

---
+++ api/test/config/tests_config.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2015 TopCoder Inc., All Rights Reserved.
+ */
+/**
+ * Represents configuration of the TopBlogger app test suite.
+ *
+ * @version 1.0
+ * @author kiri4a
+ */
+"use strict";
+
+module.exports = {
+    // Some test require valid JWT.
+    // Replace this with valid JWT otherwise some tests will fail.
+    JWT: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Imtpa28iLCJoYW5kbGUiOiJraXJpNGEiLCJpYXQiOjE0Mjc5NTA0MDR9.ncvNbGjyZMWzqvhD3fBjcjyv0Uu9QMoe12KodBW16T0'
+};
